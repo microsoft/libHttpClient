9d915f495af910f03a2e1373ae258a5f7e25d159
Jason Sandlin <jasonsa@microsoft.com>
2017-06-11 19:12:52 -0700

    Adding CMake support (#1)

---
+++ Utilities/VSOBuildScripts/postBuildScript.cmd
@@ -0,0 +1,55 @@
+if "%1" == "local" goto testlocal
+goto start
+
+:testlocal
+set TFS_DropLocation=c:\test
+mkdir %TFS_DropLocation%
+set TFS_VersionNumber=1701.10000
+set TFS_SourcesDirectory=%CD%\..\..
+goto serializeForPostbuild
+
+:start
+if "%XES_SERIALPOSTBUILDREADY%" == "True" goto serializeForPostbuild
+goto done
+:serializeForPostbuild
+
+echo Running postBuildScript.cmd
+echo on
+
+if "%BUILD_DEFINITIONNAME%" == "XSAPI_Rolling" goto finalize
+
+setlocal
+call %TFS_SourcesDirectory%\Utilities\VSOBuildScripts\setBuildVersion.cmd
+
+rem format release numbers
+for /f "tokens=2 delims==" %%G in ('wmic os get localdatetime /value') do set datetime=%%G
+set DATETIME_YEAR=%datetime:~0,4%
+set DATETIME_MONTH=%datetime:~4,2%
+set DATETIME_DAY=%datetime:~6,2%
+
+FOR /F "TOKENS=1 eol=/ DELIMS=. " %%A IN ("%TFS_VersionNumber%") DO SET SDK_POINT_NAME_YEARMONTH=%%A
+FOR /F "TOKENS=2 eol=/ DELIMS=. " %%A IN ("%TFS_VersionNumber%") DO SET SDK_POINT_NAME_DAYVER=%%A
+set SDK_POINT_NAME_YEAR=%DATETIME_YEAR%
+set SDK_POINT_NAME_MONTH=%DATETIME_MONTH%
+set SDK_POINT_NAME_DAY=%DATETIME_DAY%
+set SDK_POINT_NAME_VER=%SDK_POINT_NAME_DAYVER:~2,9%
+set SDK_RELEASE_NAME=%SDK_RELEASE_YEAR:~2,2%%SDK_RELEASE_MONTH%
+set LONG_SDK_RELEASE_NAME=%SDK_RELEASE_NAME%-%SDK_POINT_NAME_YEAR%%SDK_POINT_NAME_MONTH%%SDK_POINT_NAME_DAY%-%SDK_POINT_NAME_VER%
+set NUGET_VERSION_NUMBER=%SDK_RELEASE_YEAR%.%SDK_RELEASE_MONTH%.%SDK_POINT_NAME_YEAR%%SDK_POINT_NAME_MONTH%%SDK_POINT_NAME_DAY%.%SDK_POINT_NAME_VER%
+set MINOR_VERSION_NUMBER=%SDK_POINT_NAME_YEAR%%SDK_POINT_NAME_MONTH%%SDK_POINT_NAME_DAY%%SDK_POINT_NAME_VER%
+
+set
+
+:finalize
+if "%1" == "local" goto skipEmail
+set MSGTITLE="BUILD: %BUILD_SOURCEVERSIONAUTHOR% %BUILD_DEFINITIONNAME% %BUILD_SOURCEBRANCH% = %agent.jobstatus%"
+set MSGBODY="%TFS_DROPLOCATION%    https://microsoft.visualstudio.com/OS/_build/index?buildId=%BUILD_BUILDID%&_a=summary"
+call \\scratch2\scratch\jasonsa\tools\send-build-email.cmd %MSGTITLE% %MSGBODY% 
+:skipEmail
+
+echo.
+echo Done postBuildScript.cmd
+echo.
+endlocal
+
+:done
---
+++ Utilities/VSOBuildScripts/preCompileScript.cmd
@@ -0,0 +1,45 @@
+echo Running preCompileScript.cmd
+
+call %TFS_SourcesDirectory%\Utilities\VSOBuildScripts\setBuildVersion.cmd
+
+for /f "tokens=2 delims==" %%G in ('wmic os get localdatetime /value') do set datetime=%%G
+
+set DATETIME_YEAR=%datetime:~0,4%
+set DATETIME_MONTH=%datetime:~4,2%
+set DATETIME_DAY=%datetime:~6,2%
+
+rem format release numbers
+FOR /F "TOKENS=1 eol=/ DELIMS=. " %%A IN ("%TFS_VersionNumber%") DO SET SDK_POINT_NAME_YEARMONTH=%%A
+FOR /F "TOKENS=2 eol=/ DELIMS=. " %%A IN ("%TFS_VersionNumber%") DO SET SDK_POINT_NAME_DAYVER=%%A
+set SDK_POINT_NAME_YEAR=%DATETIME_YEAR%
+set SDK_POINT_NAME_MONTH=%DATETIME_MONTH%
+set SDK_POINT_NAME_DAY=%DATETIME_DAY%
+set SDK_POINT_NAME_VER=%SDK_POINT_NAME_DAYVER:~2,9%
+
+set SDK_RELEASE_NAME=%SDK_RELEASE_YEAR:~2,2%%SDK_RELEASE_MONTH%
+set LONG_SDK_RELEASE_NAME=%SDK_RELEASE_NAME%-%SDK_POINT_NAME_YEAR%%SDK_POINT_NAME_MONTH%%SDK_POINT_NAME_DAY%-%SDK_POINT_NAME_VER%
+set NUGET_VERSION_NUMBER=%SDK_RELEASE_YEAR%.%SDK_RELEASE_MONTH%.%SDK_POINT_NAME_YEAR%%SDK_POINT_NAME_MONTH%%SDK_POINT_NAME_DAY%.%SDK_POINT_NAME_VER%
+
+set BUILD_VERSION_FILE=%TFS_SourcesDirectory%\Source\Common\build_version.h
+
+del %BUILD_VERSION_FILE%
+echo //********************************************************* > %BUILD_VERSION_FILE%
+echo //>> %BUILD_VERSION_FILE%
+echo // Copyright (c) Microsoft. All rights reserved.>> %BUILD_VERSION_FILE%
+echo // THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF>> %BUILD_VERSION_FILE%
+echo // ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY>> %BUILD_VERSION_FILE%
+echo // IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR>> %BUILD_VERSION_FILE%
+echo // PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.>> %BUILD_VERSION_FILE%
+echo //>> %BUILD_VERSION_FILE%
+echo //*********************************************************>> %BUILD_VERSION_FILE%
+echo #pragma once>> %BUILD_VERSION_FILE%
+echo. >> %BUILD_VERSION_FILE%
+echo #define API_VERSION_STRING _T("%NUGET_VERSION_NUMBER%") >> %BUILD_VERSION_FILE%
+type %BUILD_VERSION_FILE%
+
+rem generate and compare against what's in git to see if any vcxproj is stale and email team if it is
+call %TFS_SourcesDirectory%\Utilities\CMake\MakeProjects.cmd %TFS_SourcesDirectory% skipCopy
+call %TFS_SourcesDirectory%\Utilities\VSOBuildScripts\CompareBuildFiles.cmd %TFS_SourcesDirectory% emailfailures
+
+echo Done preCompileScript.cmd
+:done
