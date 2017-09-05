fa9429bb6fdd6b6e7373359d0e9fc4a9494cc43d
Luca Beltrami <lubeltra@microsoft.com>
2017-09-05 12:54:02 -0700

    Refactor trace header to separate interface and implementation

--- Include/httpClient/trace.h
+++ Include/httpClient/trace.h
@@ -8,31 +8,46 @@
 extern "C" {
 #endif
 
-    // Configuration
-    // All macros used for configuration are always defined. Clients can define
-    // their own values for these macros to configure tracing
-    //
-    // HC_TRACE_BUILD_LEVEL [trace level (0-5)]
-    //     controls the maximum level of verbosity that will be built in the
-    //     executable. To control verbosity at runtime see TraceArea. Set to 0 to 
-    //     completely disable tracing
-    //
-    // HC_TRACE_TO_DEBUGGER [0,1]
-    //     controls if trace will output using OutputDebugString
-    //     only has an effect when building trace.cpp
-    //
-    // HC_TRACE_TO_TRACELOGGING [0,1]
-    //     controls if trace will output using the TraceLoggingProvider
-    //     only has an effect when building trace.cpp
-
-    // these defines are not meant to be used by clients, the TraceLevel enum should
-    // be used instead
+// these defines are not meant to be used by clients at runtime, the
+// HCTraceLevel enum should be used instead
 #define HC_PRIVATE_TRACE_LEVEL_OFF 0
 #define HC_PRIVATE_TRACE_LEVEL_ERROR 1
 #define HC_PRIVATE_TRACE_LEVEL_WARNING 2
 #define HC_PRIVATE_TRACE_LEVEL_IMPORTANT 3
 #define HC_PRIVATE_TRACE_LEVEL_INFORMATION 4
 #define HC_PRIVATE_TRACE_LEVEL_VERBOSE 5
+
+//------------------------------------------------------------------------------
+// trace level enum
+//------------------------------------------------------------------------------
+enum HCTraceLevel
+{
+    HC_TRACELEVEL_OFF = HC_PRIVATE_TRACE_LEVEL_OFF,
+    HC_TRACELEVEL_ERROR = HC_PRIVATE_TRACE_LEVEL_ERROR,
+    HC_TRACELEVEL_WARNING = HC_PRIVATE_TRACE_LEVEL_WARNING,
+    HC_TRACELEVEL_IMPORTANT = HC_PRIVATE_TRACE_LEVEL_IMPORTANT,
+    HC_TRACELEVEL_INFORMATION = HC_PRIVATE_TRACE_LEVEL_INFORMATION,
+    HC_TRACELEVEL_VERBOSE = HC_PRIVATE_TRACE_LEVEL_VERBOSE,
+};
+
+//------------------------------------------------------------------------------
+// Configuration
+//------------------------------------------------------------------------------
+// All macros used for configuration are always defined. Clients can define
+// their own values for these macros to control tracing behaviour
+//
+// HC_TRACE_BUILD_LEVEL [trace level (0-5)]
+//     controls the maximum level of verbosity that will be built in the
+//     executable. To control verbosity at runtime see TraceArea. Set to 0 to 
+//     completely disable tracing
+//
+// HC_TRACE_TO_DEBUGGER [0,1]
+//     controls if trace will output using OutputDebugString
+//     only has an effect when building trace.cpp
+//
+// HC_TRACE_TO_TRACELOGGING [0,1]
+//     controls if trace will output using the TraceLoggingProvider
+//     only has an effect when building trace.cpp
 
 #ifndef HC_TRACE_BUILD_LEVEL
 #define HC_TRACE_BUILD_LEVEL HC_PRIVATE_TRACE_LEVEL_VERBOSE
@@ -46,19 +61,21 @@ extern "C" {
 #define HC_TRACE_TO_CLIENT 1
 #endif
 
-    // level enabled macros, can be used to easily check if a given trace level will
-    // be included in the build (for example to avoid compiling helper code for a
-    // trace)
-    //
-    // HC_TRACE_ENABLE
-    // HC_TRACE_ERROR_ENABLE
-    // HC_TRACE_WARNING_ENABLE
-    // HC_TRACE_IMPORTANT_ENABLE
-    // HC_TRACE_INFORMATION_ENABLE
-    // HC_TRACE_VERBOSE_ENABLE
-    //
-    // these should not be manually modified, they are automatically set based on
-    // the values of HC_TRACE_ENABLE and HC_TRACE_BUILD_LEVEL
+//------------------------------------------------------------------------------
+// level enabled macros
+//------------------------------------------------------------------------------
+// these macros can be used to easily check if a given trace level will be
+// included in the build (for example to avoid compiling helper code for a trace)
+//
+// HC_TRACE_ENABLE
+// HC_TRACE_ERROR_ENABLE
+// HC_TRACE_WARNING_ENABLE
+// HC_TRACE_IMPORTANT_ENABLE
+// HC_TRACE_INFORMATION_ENABLE
+// HC_TRACE_VERBOSE_ENABLE
+//
+// these should not be manually modified, they are automatically set based on
+// the values of HC_TRACE_BUILD_LEVEL
 #if HC_TRACE_BUILD_LEVEL > HC_PRIVATE_TRACE_LEVEL_OFF
 #define HC_TRACE_ENABLE 1
 #else
@@ -99,110 +116,76 @@ extern "C" {
 #define HC_TRACE_VERBOSE_ENABLE 0
 #endif
 
-#if defined(__cplusplus)
+//------------------------------------------------------------------------------
+// Global init macros
+//------------------------------------------------------------------------------
+// Initialize, cleanup and register callback for tracing
+
+typedef void (HCTraceCallback)(
+    char const* areaName,
+    enum HCTraceLevel level,
+    unsigned int threadId,
+    uint64_t timestamp,
+    char const* message
+);
+
 #if HC_TRACE_ENABLE
-#pragma detect_mismatch("HCTrace", "on")
+#define HC_TRACE_GLOBAL_INIT() HCTraceImplGlobalInit()
+#define HC_TRACE_GLOBAL_CLEANUP() HCTraceImplGlobalCleanup()
+#define HC_TRACE_SET_CLIENT_CALLBACK(c) HCTraceImplSetClientCallback(c)
 #else
-#pragma detect_mismatch("HCTrace", "off")
+#define HC_TRACE_GLOBAL_INIT()
+#define HC_TRACE_GLOBAL_CLEANUP()
+#define HC_TRACE_SET_CLIENT_CALLBACK(c)
 #endif
-#endif
 
-    enum HCTraceLevel
-    {
-        HC_TRACELEVEL_OFF = HC_PRIVATE_TRACE_LEVEL_OFF,
-        HC_TRACELEVEL_ERROR = HC_PRIVATE_TRACE_LEVEL_ERROR,
-        HC_TRACELEVEL_WARNING = HC_PRIVATE_TRACE_LEVEL_WARNING,
-        HC_TRACELEVEL_IMPORTANT = HC_PRIVATE_TRACE_LEVEL_IMPORTANT,
-        HC_TRACELEVEL_INFORMATION = HC_PRIVATE_TRACE_LEVEL_INFORMATION,
-        HC_TRACELEVEL_VERBOSE = HC_PRIVATE_TRACE_LEVEL_VERBOSE,
-    };
-
-    struct HCTraceArea
-    {
-        char const* const Name;
-        enum HCTraceLevel Verbosity;
-    };
-
-    typedef void (HCTraceCallback)(
-        char const* areaName,
-        enum HCTraceLevel level,
-        unsigned int threadId,
-        uint64_t timestamp,
-        char const* message
-        );
-
-    void HCTraceSetClientCallback(HCTraceCallback* callback);
-
+//------------------------------------------------------------------------------
+// Trace area macros
+//------------------------------------------------------------------------------
+// these macros are used to set up areas for tracing
 #if HC_TRACE_ENABLE
-    void HCTraceMessage(
-        struct HCTraceArea const* area,
-        enum HCTraceLevel level,
-        _Printf_format_string_ char const* format,
-        ...
-    );
-
-    void HCTraceGlobalInit();
-    void HCTraceGlobalCleanup();
-
-#define HC_PRIVATE_TRACE_AREA_NAME(area) g_trace##area
-
-    // Defines an area for tracing:
-    // name is used as a prefix for all traces in the area
-    // verbosity sets the maximum verbosity that will be traced within this area
-    // vebosity can be tweaked at runtime by changing the value of
-    // g_trace<NAME>.Verbosity from the debugger (it is not intended to be changed
-    // from code)
-    // Since this defines a global variable, it should be only used from a .cpp file
-    // and each area should be defined only once
+// Defines an area for tracing:
+// name is used as a prefix for all traces in the area
+// verbosity sets the maximum verbosity that will be traced within this area
+// vebosity can be tweaked at runtime by calling HC_TRACE_SET_VERBOSITY(area,
+// level)
+// since this defines a global variable, it should be only used from a .cpp file
+// and each area should be defined only once
 #define HC_DEFINE_TRACE_AREA(area, verbosity) \
-    struct HCTraceArea HC_PRIVATE_TRACE_AREA_NAME(area) = { #area, (verbosity) }
+    struct HCTraceImplArea HC_PRIVATE_TRACE_AREA_NAME(area) = { #area, (verbosity) }
 
-    // Declares a trace area. Since DEFINE_TRACE_AREA can only be used once and in a
-    // .cpp, this allows to trace in an already defined area from another file
+// Declares a trace area. Since DEFINE_TRACE_AREA can only be used once and in a
+// .cpp, this allows to trace in an already defined area from another file
 #define HC_DECLARE_TRACE_AREA(area) \
-    extern struct HCTraceArea HC_PRIVATE_TRACE_AREA_NAME(area)
+    extern struct HCTraceImplArea HC_PRIVATE_TRACE_AREA_NAME(area)
 
-    // Initialize, cleanup and register callback for tracing
-#define HC_TRACE_GLOBAL_INIT() HCTraceGlobalInit()
-#define HC_TRACE_GLOBAL_CLEANUP() HCTraceGlobalCleanup()
-
-#define HC_TRACE_MESSAGE(area, level, ...) \
-    HCTraceMessage(&HC_PRIVATE_TRACE_AREA_NAME(area), (level), ##__VA_ARGS__)
-
-#define HC_TRACE_MESSAGE_WITH_LOCATION(area, level, format, ...) \
-    HC_TRACE_MESSAGE(HC_PRIVATE_TRACE_AREA_NAME(area), (level), format "\n    %s(%d): %s()" , ##__VA_ARGS__,  __FILE__, __LINE__, __FUNCTION__)
+#define HC_TRACE_SET_VERBOSITY(area, verbosity) \
+    HC_PRIVATE_TRACE_AREA_NAME(area).Verbosity = (verbosity)
 #else
 #define HC_DEFINE_TRACE_AREA(name, verbosity)
 #define HC_DECLARE_TRACE_AREA(name)
-
-#define HC_TRACE_GLOBAL_INIT()
-#define HC_TRACE_GLOBAL_CLEANUP()
-
-#define HC_TRACE_MESSAGE(area, level, ...)
-#define HC_TRACE_MESSAGE_WITH_LOCATION(area, level, format, ...)
 #endif
 
-#if defined(__cplusplus)
-    class HCTraceScopeHelper
-    {
-    public:
-        HCTraceScopeHelper(HCTraceArea const* area, HCTraceLevel level, char const* scope);
-        ~HCTraceScopeHelper();
-
-    private:
-        HCTraceArea const* m_area;
-        HCTraceLevel const m_level;
-        char const* const m_scope;
-        unsigned long long const m_id;
-    };
-
+//------------------------------------------------------------------------------
+// Trace macros
+//------------------------------------------------------------------------------
+// these are the macros to be used to log
 #if HC_TRACE_ENABLE
+#define HC_TRACE_MESSAGE(area, level, ...) \
+    HCTraceImplMessage(&HC_PRIVATE_TRACE_AREA_NAME(area), (level), ##__VA_ARGS__)
+
+#define HC_TRACE_MESSAGE_WITH_LOCATION(area, level, format, ...) \
+    HC_TRACE_MESSAGE(HC_PRIVATE_TRACE_AREA_NAME(area), (level), format "\n    %s(%d): %s()" , ##__VA_ARGS__,  __FILE__, __LINE__, __FUNCTION__)
+
+#ifdef __cplusplus
 #define HC_TRACE_SCOPE(area, level) \
-        auto tsh = HCTraceScopeHelper{ HC_PRIVATE_TRACE_AREA_NAME(area), level, __FUNCTION__ }
+        auto tsh = HCTraceImplScopeHelper{ HC_PRIVATE_TRACE_AREA_NAME(area), level, __FUNCTION__ }
 #else
 #define HC_TRACE_SCOPE(area, level)
 #endif
 #else
+#define HC_TRACE_MESSAGE(area, level, ...)
+#define HC_TRACE_MESSAGE_WITH_LOCATION(area, level, format, ...)
 #define HC_TRACE_SCOPE(area, level)
 #endif
 
@@ -262,6 +245,44 @@ extern "C" {
 #else
 #define HC_TRACE_VERBOSE(area, msg, ...)
 #define HC_TRACE_SCOPE_VERBOSE(area)
+#endif
+
+//------------------------------------------------------------------------------
+// Implementation
+//------------------------------------------------------------------------------
+#define HC_PRIVATE_TRACE_AREA_NAME(area) g_trace##area
+
+struct HCTraceImplArea
+{
+    char const* const Name;
+    enum HCTraceLevel Verbosity;
+};
+
+void HCTraceImplGlobalInit();
+void HCTraceImplGlobalCleanup();
+
+void HCTraceImplSetClientCallback(HCTraceCallback* callback);
+
+void HCTraceImplMessage(
+    struct HCTraceImplArea const* area,
+    enum HCTraceLevel level,
+    _Printf_format_string_ char const* format,
+    ...
+);
+
+#if defined(__cplusplus)
+class HCTraceImplScopeHelper
+{
+public:
+    HCTraceImplScopeHelper(HCTraceImplArea const* area, HCTraceLevel level, char const* scope);
+    ~HCTraceImplScopeHelper();
+
+private:
+    HCTraceImplArea const* m_area;
+    HCTraceLevel const m_level;
+    char const* const m_scope;
+    unsigned long long const m_id;
+};
 #endif
 
 #if defined(__cplusplus)
--- Source/Logger/trace.cpp
+++ Source/Logger/trace.cpp
@@ -16,168 +16,168 @@
 namespace
 {
 
-    class TraceState
+class TraceState
+{
+public:
+    void Init()
     {
-    public:
-        void Init()
+        auto previousCount = m_tracingClients.fetch_add(1);
+        if (previousCount == 0)
         {
-            auto previousCount = m_tracingClients.fetch_add(1);
-            if (previousCount == 0)
-            {
-                m_initTime = std::chrono::high_resolution_clock::now();
-            }
+            m_initTime = std::chrono::high_resolution_clock::now();
         }
+    }
 
-        void Cleanup()
-        {
-            --m_tracingClients;
-        }
+    void Cleanup()
+    {
+        --m_tracingClients;
+    }
 
-        bool IsSetup() const
-        {
-            return m_tracingClients > 0;
-        }
+    bool IsSetup() const
+    {
+        return m_tracingClients > 0;
+    }
 
-        void SetClientCallback(HCTraceCallback* callback)
-        {
-            m_clientCallback = callback;
-        }
+    void SetClientCallback(HCTraceCallback* callback)
+    {
+        m_clientCallback = callback;
+    }
 
-        HCTraceCallback* GetClientCallback() const
-        {
-            return m_clientCallback;
-        }
+    HCTraceCallback* GetClientCallback() const
+    {
+        return m_clientCallback;
+    }
 
-        uint64_t GetTimestamp() const
-        {
-            auto now = std::chrono::high_resolution_clock::now();
-            auto nowMS = std::chrono::duration_cast<std::chrono::milliseconds>(now - m_initTime.load());
-            return nowMS.count();
-        }
+    uint64_t GetTimestamp() const
+    {
+        auto now = std::chrono::high_resolution_clock::now();
+        auto nowMS = std::chrono::duration_cast<std::chrono::milliseconds>(now - m_initTime.load());
+        return nowMS.count();
+    }
 
-    private:
-        std::atomic<uint32_t> m_tracingClients = 0;
-        std::atomic<std::chrono::high_resolution_clock::time_point> m_initTime =
-            std::chrono::high_resolution_clock::now();
-        std::atomic<HCTraceCallback*> m_clientCallback = nullptr;
+private:
+    std::atomic<uint32_t> m_tracingClients = 0;
+    std::atomic<std::chrono::high_resolution_clock::time_point> m_initTime =
+        std::chrono::high_resolution_clock::now();
+    std::atomic<HCTraceCallback*> m_clientCallback = nullptr;
+};
+
+TraceState g_traceState;
+
+void TraceMessageToDebugger(
+    char const* areaName,
+    HCTraceLevel level,
+    unsigned int threadId,
+    uint64_t timestamp,
+    char const* message
+)
+{
+#if HC_TRACE_TO_DEBUGGER
+    // Needs to match the HCTraceLevel enum
+    static char const* traceLevelNames[] =
+    {
+        "Off",
+        "E",
+        "W",
+        "P",
+        "I",
+        "V"
     };
 
-    TraceState g_traceState;
+    static size_t const BUFFER_SIZE = 4096;
 
-    void TraceMessageToDebugger(
-        char const* areaName,
-        HCTraceLevel level,
-        unsigned int threadId,
-        uint64_t timestamp,
-        char const* message
-    )
+    std::time_t  timeTInSec = static_cast<std::time_t>(timestamp / 1000);
+    uint32_t     fractionMSec = static_cast<uint32_t>(timestamp % 1000);
+    std::tm      fmtTime = {};
+    localtime_s(&fmtTime, &timeTInSec);
+
+    char outputBuffer[BUFFER_SIZE] = {};
+    // [threadId][level][time][area] message
+    auto written = sprintf_s(outputBuffer, "[%04X][%hs][%02d:%02d:%02d.%03u][%hs] %hs",
+        threadId,
+        traceLevelNames[static_cast<size_t>(level)],
+        fmtTime.tm_hour,
+        fmtTime.tm_min,
+        fmtTime.tm_sec,
+        fractionMSec,
+        areaName,
+        message
+    );
+    if (written <= 0)
     {
-#if HC_TRACE_TO_DEBUGGER
-        // Needs to match the HCTraceLevel enum
-        static char const* traceLevelNames[] =
-        {
-            "Off",
-            "E",
-            "W",
-            "P",
-            "I",
-            "V"
-        };
-
-        static size_t const BUFFER_SIZE = 4096;
-
-        std::time_t  timeTInSec = static_cast<std::time_t>(timestamp / 1000);
-        uint32_t     fractionMSec = static_cast<uint32_t>(timestamp % 1000);
-        std::tm      fmtTime = {};
-        localtime_s(&fmtTime, &timeTInSec);
-
-        char outputBuffer[BUFFER_SIZE] = {};
-        // [threadId][level][time][area] message
-        auto written = sprintf_s(outputBuffer, "[%04X][%hs][%02d:%02d:%02d.%03u][%hs] %hs",
-            threadId,
-            traceLevelNames[static_cast<size_t>(level)],
-            fmtTime.tm_hour,
-            fmtTime.tm_min,
-            fmtTime.tm_sec,
-            fractionMSec,
-            areaName,
-            message
-        );
-        if (written <= 0)
-        {
-            return;
-        }
-
-        // Make sure there is room for the \r \n and \0
-        written = std::min(written, static_cast<int>(BUFFER_SIZE - 3));
-        auto remaining = BUFFER_SIZE - written;
-
-        // Print new line
-        auto written2 = sprintf_s(outputBuffer + written, remaining, "\r\n");
-        if (written2 <= 0)
-        {
-            return;
-        }
-
-        OutputDebugStringA(outputBuffer);
-#else
-        (void)areaName;
-        (void)level;
-        (void)threadId;
-        (void)timestamp;
-        (void)message;
-#endif
+        return;
     }
 
-    void TraceMessageToClient(
-        char const* areaName,
-        HCTraceLevel level,
-        unsigned int threadId,
-        uint64_t timestamp,
-        char const* message
-    )
+    // Make sure there is room for the \r \n and \0
+    written = std::min(written, static_cast<int>(BUFFER_SIZE - 3));
+    auto remaining = BUFFER_SIZE - written;
+
+    // Print new line
+    auto written2 = sprintf_s(outputBuffer + written, remaining, "\r\n");
+    if (written2 <= 0)
     {
+        return;
+    }
+
+    OutputDebugStringA(outputBuffer);
+#else
+    (void)areaName;
+    (void)level;
+    (void)threadId;
+    (void)timestamp;
+    (void)message;
+#endif
+}
+
+void TraceMessageToClient(
+    char const* areaName,
+    HCTraceLevel level,
+    unsigned int threadId,
+    uint64_t timestamp,
+    char const* message
+)
+{
 #if HC_TRACE_TO_CLIENT
-        auto callback = g_traceState.GetClientCallback();
-        if (callback)
-        {
-            callback(areaName, level, threadId, timestamp, message);
-        }
-#else
-        (void)areaName;
-        (void)level;
-        (void)threadId;
-        (void)timestamp;
-        (void)message;
-#endif
-    }
-
-    unsigned long long GetScopeId()
+    auto callback = g_traceState.GetClientCallback();
+    if (callback)
     {
-        LARGE_INTEGER li = {};
-        QueryPerformanceCounter(&li);
-        return li.QuadPart;
+        callback(areaName, level, threadId, timestamp, message);
     }
+#else
+    (void)areaName;
+    (void)level;
+    (void)threadId;
+    (void)timestamp;
+    (void)message;
+#endif
+}
+
+unsigned long long GetScopeId()
+{
+    LARGE_INTEGER li = {};
+    QueryPerformanceCounter(&li);
+    return li.QuadPart;
+}
 
 }
 
-void HCTraceGlobalInit()
+void HCTraceImplGlobalInit()
 {
     g_traceState.Init();
 }
 
-void HCTraceGlobalCleanup()
+void HCTraceImplGlobalCleanup()
 {
     g_traceState.Cleanup();
 }
 
-void HCTraceSetClientCallback(HCTraceCallback* callback)
+void HCTraceImplSetClientCallback(HCTraceCallback* callback)
 {
     g_traceState.SetClientCallback(callback);
 }
 
-void HCTraceMessage(
-    struct HCTraceArea const* area,
+void HCTraceImplMessage(
+    struct HCTraceImplArea const* area,
     enum HCTraceLevel level,
     _Printf_format_string_ char const* format,
     ...
@@ -218,13 +218,13 @@ void HCTraceMessage(
     TraceMessageToClient(area->Name, level, threadId, timestamp, message);
 }
 
-HCTraceScopeHelper::HCTraceScopeHelper(HCTraceArea const* area, HCTraceLevel level, char const* scope)
+HCTraceImplScopeHelper::HCTraceImplScopeHelper(HCTraceImplArea const* area, HCTraceLevel level, char const* scope)
     : m_area{ area }, m_level{ level }, m_scope{ scope }, m_id{ GetScopeId() }
 {
-    HCTraceMessage(m_area, m_level, ">>> %hs (%016llX)", m_scope, m_id);
+    HCTraceImplMessage(m_area, m_level, ">>> %hs (%016llX)", m_scope, m_id);
 }
 
-HCTraceScopeHelper::~HCTraceScopeHelper()
+HCTraceImplScopeHelper::~HCTraceImplScopeHelper()
 {
-    HCTraceMessage(m_area, m_level, "<<< %hs (%016llX)", m_scope, m_id);
+    HCTraceImplMessage(m_area, m_level, "<<< %hs (%016llX)", m_scope, m_id);
 }
