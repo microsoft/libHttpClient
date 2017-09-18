14ab06610bf17c72f2b4da0ce160f7ac93ea02e8
Luca Beltrami <lubeltra@microsoft.com>
2017-09-18 15:47:30 -0700

    Updated string conversions (#54)
    * Refactoring mem.h
    
    - Cut down http_stl_allocator to the C++11 minimal allocator interface
    - Replaced the http_internal_* container macros with template aliases
    - Replaced direct uses of stl containers with the http_interal_* version in a couple of places
    - Slight cleanup of pch.h
    
    * Updated string conversions utilities to be platform specific and to use non deprecated methods on Windows.
    Replaced references to default standard containers with custom allocated versions

--- Source/HTTP/WinRTHttpClient/http_winrthttpclient.cpp
+++ Source/HTTP/WinRTHttpClient/http_winrthttpclient.cpp
@@ -52,10 +52,10 @@ void uwp_http_task::perform_async(
 {
     try
     {
-        const CHAR* url = nullptr;
-        const CHAR* method = nullptr;
-        const CHAR* requestBody = nullptr;
-        const CHAR* userAgent = nullptr;
+        const char* url = nullptr;
+        const char* method = nullptr;
+        const char* requestBody = nullptr;
+        const char* userAgent = nullptr;
         HCHttpCallRequestGetUrl(call, &method, &url);
         HCHttpCallRequestGetRequestBodyString(call, &requestBody);
 
@@ -63,8 +63,8 @@ void uwp_http_task::perform_async(
         HCHttpCallRequestGetNumHeaders(call, &numHeaders);
 
         HttpClient^ httpClient = ref new HttpClient();
-        std::wstring wUrl = xbox::httpclient::to_wstring(url);
-        std::wstring wMethod = xbox::httpclient::to_wstring(method);
+        http_internal_wstring wUrl = utf16_from_utf8(url);
+        http_internal_wstring wMethod = utf16_from_utf8(method);
         Uri^ requestUri = ref new Uri(ref new Platform::String(wUrl.c_str()));
         HttpRequestMessage^ requestMsg = ref new HttpRequestMessage(ref new HttpMethod(ref new Platform::String(wMethod.c_str())), requestUri);
 
@@ -72,26 +72,26 @@ void uwp_http_task::perform_async(
 
         for (uint32_t i = 0; i < numHeaders; i++)
         {
-            const CHAR* headerName;
-            const CHAR* headerValue;
+            const char* headerName;
+            const char* headerValue;
             HCHttpCallRequestGetHeaderAtIndex(call, i, &headerName, &headerValue);
             if (headerName != nullptr && headerValue != nullptr)
             {
-                std::wstring wHeaderName = xbox::httpclient::to_wstring(headerName);
-                std::wstring wHeaderValue = xbox::httpclient::to_wstring(headerValue);
+                http_internal_wstring wHeaderName = utf16_from_utf8(headerName);
+                http_internal_wstring wHeaderValue = utf16_from_utf8(headerValue);
                 requestMsg->Headers->TryAppendWithoutValidation(ref new Platform::String(wHeaderName.c_str()), ref new Platform::String(wHeaderValue.c_str()
                 ));
             }
         }
 
-        requestMsg->Headers->AcceptEncoding->TryParseAdd(ref new Platform::String(L"gzip"));
-        requestMsg->Headers->AcceptEncoding->TryParseAdd(ref new Platform::String(L"deflate"));
-        requestMsg->Headers->AcceptEncoding->TryParseAdd(ref new Platform::String(L"br"));
-        requestMsg->Headers->Accept->TryParseAdd(ref new Platform::String(L"*/*"));
+        requestMsg->Headers->AcceptEncoding->TryParseAdd(Platform::StringReference(L"gzip"));
+        requestMsg->Headers->AcceptEncoding->TryParseAdd(Platform::StringReference(L"deflate"));
+        requestMsg->Headers->AcceptEncoding->TryParseAdd(Platform::StringReference(L"br"));
+        requestMsg->Headers->Accept->TryParseAdd(Platform::StringReference(L"*/*"));
 
         if (requestBody != nullptr)
         {
-            std::wstring wRequestBody = xbox::httpclient::to_wstring(requestBody);
+            http_internal_wstring wRequestBody = utf16_from_utf8(requestBody);
             requestMsg->Content = ref new HttpStringContent(ref new Platform::String(wRequestBody.c_str()
             ));
             requestMsg->Content->Headers->ContentType = Windows::Web::Http::Headers::HttpMediaTypeHeaderValue::Parse(L"application/json; charset=utf-8");
@@ -117,8 +117,8 @@ void uwp_http_task::perform_async(
                     auto cur = iter->Current;
                     auto headerName = cur->Key;
                     auto headerValue = cur->Value;
-                    std::string aHeaderName = xbox::httpclient::to_utf8string(headerName->Data());
-                    std::string aHeaderValue = xbox::httpclient::to_utf8string(headerValue->Data());
+                    http_internal_string aHeaderName = utf8_from_utf16(headerName->Data());
+                    http_internal_string aHeaderValue = utf8_from_utf16(headerValue->Data());
                     HCHttpCallResponseSetHeader(call, aHeaderName.c_str(), aHeaderValue.c_str());
                 }
 
@@ -129,7 +129,7 @@ void uwp_http_task::perform_async(
                     try
                     {
                         Platform::String^ httpResponseBody = asyncOp->GetResults();
-                        std::string aHttpResponseBody = xbox::httpclient::to_utf8string(httpResponseBody->Data());
+                        http_internal_string aHttpResponseBody = utf8_from_utf16(httpResponseBody->Data());
                         HCHttpCallResponseSetResponseString(call, aHttpResponseBody.c_str());
                         HCTaskSetCompleted(taskHandle);
                     }
