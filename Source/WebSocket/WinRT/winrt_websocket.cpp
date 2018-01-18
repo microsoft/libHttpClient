c4583da3d7961b1f7f4e76995f1ab532bea87c71
Ramsey Khadder <rakhad@microsoft.com>
2018-01-17 17:25:51 -0800

    Updated proj files

--- Source/WebSocket/WinRT/winrt_websocket.cpp
+++ Source/WebSocket/WinRT/winrt_websocket.cpp
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include "pch.h"
-#include "websocket.h"
+#include "..\hcwebsocket.h"
 
 using namespace xbox::httpclient;
 
