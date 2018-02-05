63057fbac1fafa7b3b8cd09cb3d42875daf0fc79
Jason Sandlin <jasonsa@microsoft.com>
2018-02-05 15:12:06 -0800

    Fixing Win32 build (#96)

--- Source/WebSocket/Win32/win32_websocket.cpp
+++ Source/WebSocket/Win32/win32_websocket.cpp
@@ -8,14 +8,14 @@ using namespace xbox::httpclient;
 
 
 HC_RESULT Internal_HCWebSocketConnect(
-    _In_z_ PCSTR uri,
-    _In_ HC_WEBSOCKET_HANDLE websocket,
-    _In_ HC_WEBSOCKET_CONNECT_INIT_ARGS args,
-    _In_ HC_SUBSYSTEM_ID taskSubsystemId,
-    _In_ uint64_t taskGroupId,
-    _In_opt_ void* completionRoutineContext,
-    _In_opt_ HCWebSocketCompletionRoutine completionRoutine
-)
+	_In_z_ PCSTR uri,
+	_In_z_ PCSTR subProtocol,
+	_In_ HC_WEBSOCKET_HANDLE websocket,
+	_In_ HC_SUBSYSTEM_ID taskSubsystemId,
+	_In_ uint64_t taskGroupId,
+	_In_opt_ void* completionRoutineContext,
+	_In_opt_ HCWebSocketCompletionRoutine completionRoutine
+	)
 {
     // TODO
     return HC_OK;
@@ -34,7 +34,7 @@ HC_RESULT Internal_HCWebSocketSendMessage(
     return HC_OK;
 }
 
-HC_RESULT Internal_HCWebSocketClose(
+HC_RESULT Internal_HCWebSocketDisconnect(
     _In_ HC_WEBSOCKET_HANDLE websocket,
     _In_ HC_WEBSOCKET_CLOSE_STATUS closeStatus
     )
