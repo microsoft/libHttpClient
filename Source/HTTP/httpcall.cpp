5d2ede1f1b69ff8710822c5df11bd8721e0826e3
John LaFontaine <johlafo@microsoft.com>
2018-08-03 15:20:34 -0700

    Adding api to get http request URI from the handle

--- Source/HTTP/httpcall.cpp
+++ Source/HTTP/httpcall.cpp
@@ -524,11 +524,33 @@ try
 {
     if (call == nullptr)
     {
-        return 0;
+        return E_INVALIDARG;
     }
     
     *context = call->context;
 
     return S_OK;
+}
+CATCH_RETURN()
+
+STDAPI 
+HCHttpCallGetRequestUrl(
+	_In_ hc_call_handle_t call,
+	_Out_ const char** url
+	) HC_NOEXCEPT
+try
+{
+	if (call == nullptr)
+	{
+		return E_INVALIDARG;
+	}
+
+	if (call->url.empty())
+	{
+		*url = nullptr;
+	}
+
+	*url = call->url.data();
+	return S_OK;
 }
 CATCH_RETURN()
