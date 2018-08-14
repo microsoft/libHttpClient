2313680064066cb2353f168b6113358117bbaf7d
John L <johlafo@microsoft.com>
2018-08-13 17:01:11 -0700

    Adding libssl.Android and adding wspp files to Android libHttpClient build.

---
+++ Source/Common/Android/utils_android.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "pch.h"
+#include "utils_android.h"
+
+NAMESPACE_XBOX_HTTP_CLIENT_BEGIN
+
+static void abort_if_no_jvm()
+{
+    if (JVM == nullptr)
+    {
+        std::abort();
+    }
+}
+
+JNIEnv* get_jvm_env()
+{
+    abort_if_no_jvm();
+    JNIEnv* env = nullptr;
+    auto result = JVM.load()->AttachCurrentThread(&env, nullptr);
+    if (result != JNI_OK)
+    {
+        throw std::runtime_error("Could not attach to JVM");
+    }
+
+    return env;
+}
+
+NAMESPACE_XBOX_HTTP_CLIENT_END
