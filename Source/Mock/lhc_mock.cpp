47ab59eca11ef09380ba52764b59d40d8e705f85
Heba Basiony <heba@microsoft.com>
2018-11-19 13:03:40 -0800

    Renaming Source/mock files to lhc_mock

--- Source/Mock/lhc_mock.cpp
+++ Source/Mock/lhc_mock.cpp
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include "pch.h"
-#include "mock.h"
+#include "lhc_mock.h"
 #include "../HTTP/httpcall.h"
 
 using namespace xbox::httpclient;
