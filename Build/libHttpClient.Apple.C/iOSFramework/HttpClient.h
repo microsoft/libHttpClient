f42528338209c87117579199ed8e017f3b521df3
Matt VanderKolk <mava@microsoft.com>
2019-02-11 15:47:36 -0800

    Making empty framework

---
+++ Build/libHttpClient.Apple.C/iOSFramework/HttpClient.h
@@ -0,0 +1,18 @@
+//
+//  HttpClient.h
+//  HttpClient
+//
+//  Created by Matt VanderKolk on 2/11/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HttpClient.
+FOUNDATION_EXPORT double HttpClientVersionNumber;
+
+//! Project version string for HttpClient.
+FOUNDATION_EXPORT const unsigned char HttpClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HttpClient/PublicHeader.h>
+
+
