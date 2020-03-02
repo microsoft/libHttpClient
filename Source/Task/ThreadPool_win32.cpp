5100751f6926c854cb44519e9515c69170037518
Brian Pepin <brianpe@microsoft.com>
2020-03-02 14:41:48 -0800

    PR feedback from Windows (#497)
    This is an update that addresses minor PR feedback from the Windows team. Mainly:
    
    Duplication of code in push_back.
    Whitespace issues.
    Also in this change:
    
    Switch from C_ASSERT to static_assert. It turns out this is supported across all platforms after all as long as you use the variant with a message.
    
    Re-arrange members so Android compiler doesn't generate warnings about initialization order.

--- Source/Task/ThreadPool_win32.cpp
+++ Source/Task/ThreadPool_win32.cpp
@@ -34,9 +34,6 @@ namespace OS
             m_work = CreateThreadpoolWork(TPCallback, this, nullptr);
             RETURN_LAST_ERROR_IF_NULL(m_work);
 
-            InitializeCriticalSection(&m_cs);
-            InitializeConditionVariable(&m_cv);
-
             return S_OK;
         }
 
@@ -44,35 +41,24 @@ namespace OS
         {
             if (m_work != nullptr)
             {
-                // We cannot wait for work callbacks to complete because
-                // the final release may be called from within the callback.
-                // We know when our callbacks are invoking user code however,
-                // and we block on that.
-                EnterCriticalSection(&m_cs);
-
-                while (m_activeCalls != 0)
-                {
-                    SleepConditionVariableCS(&m_cv, &m_cs, INFINITE);
-                }
-
-                LeaveCriticalSection(&m_cs);
-
+                // Wait for callbacks to complete.  We don't want to
+                // cancel existing callbacks because it's important they
+                // get a chance to drain.
+                WaitForThreadpoolWorkCallbacks(m_work, FALSE);
                 CloseThreadpoolWork(m_work);
                 m_work = nullptr;
-                DeleteCriticalSection(&m_cs);
             }
         }
 
         void Submit() noexcept
         {
-            m_activeCalls++;
             SubmitThreadpoolWork(m_work);
         }
 
     private:
 
         static void CALLBACK TPCallback(
-            _In_ PTP_CALLBACK_INSTANCE,
+            _In_ PTP_CALLBACK_INSTANCE instance,
             _In_ void* context, PTP_WORK) noexcept
         {
             ThreadPoolImpl* pthis = static_cast<ThreadPoolImpl*>(context);
@@ -90,7 +76,7 @@ namespace OS
             // final release does need to wait on outstanding
             // calls.
 
-            ActionCompleteImpl ac(pthis);
+            ActionCompleteImpl ac(pthis, instance);
             pthis->AddRef();
             pthis->m_callback(pthis->m_context, ac);
 
@@ -103,8 +89,9 @@ namespace OS
 
         struct ActionCompleteImpl : ThreadPoolActionComplete
         {
-            ActionCompleteImpl(ThreadPoolImpl* owner) :
-                m_owner(owner)
+            ActionCompleteImpl(ThreadPoolImpl* owner, PTP_CALLBACK_INSTANCE instance) :
+                m_owner(owner),
+                m_instance(instance)
             {
             }
 
@@ -113,20 +100,17 @@ namespace OS
             void operator()() override
             {
                 Invoked = true;
-                m_owner->m_activeCalls--;
-                WakeAllConditionVariable(&m_owner->m_cv);
+                DisassociateCurrentThreadFromCallback(m_instance);
             }
 
         private:
             ThreadPoolImpl* m_owner = nullptr;
+            PTP_CALLBACK_INSTANCE m_instance = nullptr;
         };
 
         std::atomic<uint32_t> m_refs{ 1 };
-        CONDITION_VARIABLE m_cv;
-        CRITICAL_SECTION m_cs;
         PTP_WORK m_work = nullptr;
         void* m_context = nullptr;
-        std::atomic<uint32_t> m_activeCalls{ 0 };
         ThreadPoolCallback* m_callback = nullptr;
     };
 
