18b9b665f237c87f776fb5417c5f50720bfbaae8
sashaweiss <asashaweiss@gmail.com>
2020-05-22 15:41:59 -0700

    Add setting release output dir

--- Utilities/CMake/Android/SetBinaryOutputDir.cmake
+++ Utilities/CMake/Android/SetBinaryOutputDir.cmake
@@ -2,4 +2,5 @@ cmake_minimum_required(VERSION 3.6)
 
 function(SET_BINARY_OUTPUT_DIR PATH_TO_ROOT)
     set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG "${PATH_TO_ROOT}/Binaries/Android/native/debug/${ANDROID_ABI}" PARENT_SCOPE)
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE "${PATH_TO_ROOT}/Binaries/Android/native/release/${ANDROID_ABI}" PARENT_SCOPE)
 endfunction()
