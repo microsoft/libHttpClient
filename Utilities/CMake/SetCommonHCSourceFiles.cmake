7a53f00fb53cc330a7fc0acbf6c5d22a9e8a8c02
sashaweiss <asashaweiss@gmail.com>
2020-05-26 12:01:28 -0700

    Tweak CMakeLists.txt to use common hc source files

--- Utilities/CMake/SetCommonHCSourceFiles.cmake
+++ Utilities/CMake/SetCommonHCSourceFiles.cmake
@@ -1,77 +1,77 @@
 cmake_minimum_required(VERSION 3.6)
 
-function(SET_COMMON_HC_SOURCE_FILES OUT_SOURCE_FILES OUT_INCLUDE_DIRS REL_PATH_TO_PROJECT_DIR)
+function(SET_COMMON_HC_SOURCE_FILES OUT_SOURCE_FILES OUT_INCLUDE_DIRS PATH_TO_ROOT)
     set(PUBLIC_SOURCE_FILES
-        "${REL_PATH_TO_PROJECT_DIR}/include/httpClient/config.h"
-        "${REL_PATH_TO_PROJECT_DIR}/include/httpClient/httpClient.h"
-        "${REL_PATH_TO_PROJECT_DIR}/include/httpClient/httpProvider.h"
-        "${REL_PATH_TO_PROJECT_DIR}/include/httpClient/mock.h"
-        "${REL_PATH_TO_PROJECT_DIR}/include/xasync.h"
-        "${REL_PATH_TO_PROJECT_DIR}/include/xasyncProvider.h"
-        "${REL_PATH_TO_PROJECT_DIR}/include/xtaskQueue.h"
-        "${REL_PATH_TO_PROJECT_DIR}/include/httpClient/trace.h"
-        "${REL_PATH_TO_PROJECT_DIR}/include/httpClient/pal.h"
-        "${REL_PATH_TO_PROJECT_DIR}/include/httpClient/async.h"
+        "${PATH_TO_ROOT}/Include/httpClient/config.h"
+        "${PATH_TO_ROOT}/Include/httpClient/httpClient.h"
+        "${PATH_TO_ROOT}/Include/httpClient/httpProvider.h"
+        "${PATH_TO_ROOT}/Include/httpClient/mock.h"
+        "${PATH_TO_ROOT}/Include/xasync.h"
+        "${PATH_TO_ROOT}/Include/xasyncProvider.h"
+        "${PATH_TO_ROOT}/Include/xtaskQueue.h"
+        "${PATH_TO_ROOT}/Include/httpClient/trace.h"
+        "${PATH_TO_ROOT}/Include/httpClient/pal.h"
+        "${PATH_TO_ROOT}/Include/httpClient/async.h"
         )
 
     set(COMMON_SOURCE_FILES
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Common/buildver.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Common/EntryList.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Common/pch.cpp"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Common/pch.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Common/pch_common.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Common/pal_internal.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Common/ResultMacros.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Common/uri.cpp"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Common/uri.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Common/utils.cpp"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Common/utils.h"
+        "${PATH_TO_ROOT}/Source/Common/buildver.h"
+        "${PATH_TO_ROOT}/Source/Common/EntryList.h"
+        "${PATH_TO_ROOT}/Source/Common/pch.cpp"
+        "${PATH_TO_ROOT}/Source/Common/pch.h"
+        "${PATH_TO_ROOT}/Source/Common/pch_common.h"
+        "${PATH_TO_ROOT}/Source/Common/pal_internal.h"
+        "${PATH_TO_ROOT}/Source/Common/ResultMacros.h"
+        "${PATH_TO_ROOT}/Source/Common/uri.cpp"
+        "${PATH_TO_ROOT}/Source/Common/uri.h"
+        "${PATH_TO_ROOT}/Source/Common/utils.cpp"
+        "${PATH_TO_ROOT}/Source/Common/utils.h"
         )
 
     set(GLOBAL_SOURCE_FILES
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Global/mem.cpp"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Global/mem.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Global/global_publics.cpp"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Global/global.cpp"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Global/global.h"
+        "${PATH_TO_ROOT}/Source/Global/mem.cpp"
+        "${PATH_TO_ROOT}/Source/Global/mem.h"
+        "${PATH_TO_ROOT}/Source/Global/global_publics.cpp"
+        "${PATH_TO_ROOT}/Source/Global/global.cpp"
+        "${PATH_TO_ROOT}/Source/Global/global.h"
         )
 
     set(WEBSOCKET_SOURCE_FILES
-        "${REL_PATH_TO_PROJECT_DIR}/Source/WebSocket/hcwebsocket.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/WebSocket/hcwebsocket.cpp"
+        "${PATH_TO_ROOT}/Source/WebSocket/hcwebsocket.h"
+        "${PATH_TO_ROOT}/Source/WebSocket/hcwebsocket.cpp"
         )
 
     set(TASK_SOURCE_FILES
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Task/AsyncLib.cpp"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Task/AtomicVector.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Task/LocklessQueue.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Task/referenced_ptr.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Task/StaticArray.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Task/TaskQueue.cpp"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Task/TaskQueueImpl.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Task/TaskQueueP.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Task/ThreadPool.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Task/WaitTimer.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Task/XTaskQueuePriv.h"
+        "${PATH_TO_ROOT}/Source/Task/AsyncLib.cpp"
+        "${PATH_TO_ROOT}/Source/Task/AtomicVector.h"
+        "${PATH_TO_ROOT}/Source/Task/LocklessQueue.h"
+        "${PATH_TO_ROOT}/Source/Task/referenced_ptr.h"
+        "${PATH_TO_ROOT}/Source/Task/StaticArray.h"
+        "${PATH_TO_ROOT}/Source/Task/TaskQueue.cpp"
+        "${PATH_TO_ROOT}/Source/Task/TaskQueueImpl.h"
+        "${PATH_TO_ROOT}/Source/Task/TaskQueueP.h"
+        "${PATH_TO_ROOT}/Source/Task/ThreadPool.h"
+        "${PATH_TO_ROOT}/Source/Task/WaitTimer.h"
+        "${PATH_TO_ROOT}/Source/Task/XTaskQueuePriv.h"
         )
 
     set(MOCK_SOURCE_FILES
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Mock/lhc_mock.cpp"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Mock/lhc_mock.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Mock/mock_publics.cpp"
+        "${PATH_TO_ROOT}/Source/Mock/lhc_mock.cpp"
+        "${PATH_TO_ROOT}/Source/Mock/lhc_mock.h"
+        "${PATH_TO_ROOT}/Source/Mock/mock_publics.cpp"
         )
 
     set(HTTP_SOURCE_FILES
-        "${REL_PATH_TO_PROJECT_DIR}/Source/HTTP/httpcall.cpp"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/HTTP/httpcall.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/HTTP/httpcall_request.cpp"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/HTTP/httpcall_response.cpp"
+        "${PATH_TO_ROOT}/Source/HTTP/httpcall.cpp"
+        "${PATH_TO_ROOT}/Source/HTTP/httpcall.h"
+        "${PATH_TO_ROOT}/Source/HTTP/httpcall_request.cpp"
+        "${PATH_TO_ROOT}/Source/HTTP/httpcall_response.cpp"
         )
 
     set(LOGGER_SOURCE_FILES
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Logger/trace.cpp"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Logger/trace_internal.h"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Logger/log_publics.cpp"
+        "${PATH_TO_ROOT}/Source/Logger/trace.cpp"
+        "${PATH_TO_ROOT}/Source/Logger/trace_internal.h"
+        "${PATH_TO_ROOT}/Source/Logger/log_publics.cpp"
         )
 
     message(STATUS "Common source group")
@@ -85,15 +85,15 @@ function(SET_COMMON_HC_SOURCE_FILES OUT_SOURCE_FILES OUT_INCLUDE_DIRS REL_PATH_T
     source_group("C++ Source\\Logger" FILES "${LOGGER_SOURCE_FILES}")
 
     set(${OUT_INCLUDE_DIRS}
-        "${REL_PATH_TO_PROJECT_DIR}/Source"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Common"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/HTTP"
-        "${REL_PATH_TO_PROJECT_DIR}/Source/Logger"
-        "${REL_PATH_TO_PROJECT_DIR}/include"
-        "${REL_PATH_TO_PROJECT_DIR}/include/httpClient"
-        "${REL_PATH_TO_PROJECT_DIR}/External/asio/asio/include"
-        "${REL_PATH_TO_PROJECT_DIR}/External/openssl/include"
-        "${REL_PATH_TO_PROJECT_DIR}/External/websocketpp"
+        "${PATH_TO_ROOT}/Source"
+        "${PATH_TO_ROOT}/Source/Common"
+        "${PATH_TO_ROOT}/Source/HTTP"
+        "${PATH_TO_ROOT}/Source/Logger"
+        "${PATH_TO_ROOT}/Include"
+        "${PATH_TO_ROOT}/Include/httpClient"
+        "${PATH_TO_ROOT}/External/asio/asio/include"
+        "${PATH_TO_ROOT}/External/openssl/include"
+        "${PATH_TO_ROOT}/External/websocketpp"
         PARENT_SCOPE
         )
 
