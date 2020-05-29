8aca74ab0701d4f634c49ac71f4a78b99b61045a
sashaweiss <asashaweiss@gmail.com>
2020-05-29 15:06:30 -0700

    Align Java and native binary dirs per feedback

--- Utilities/CMake/Android/GetLibHCBinaryOutputDir.cmake
+++ Utilities/CMake/Android/GetLibHCBinaryOutputDir.cmake
@@ -2,5 +2,5 @@ cmake_minimum_required(VERSION 3.6)
 
 function(GET_LIBHC_BINARY_OUTPUT_DIR PATH_TO_ROOT OUT_BINARY_DIR)
     string(TOLOWER "${CMAKE_BUILD_TYPE}" PATH_FLAVOR)
-    set(${OUT_BINARY_DIR} "${PATH_TO_ROOT}/Binaries/Android/native/${PATH_FLAVOR}/${ANDROID_ABI}" PARENT_SCOPE)
+    set(${OUT_BINARY_DIR} "${PATH_TO_ROOT}/Binaries/Android/${ANDROID_ABI}/${PATH_FLAVOR}" PARENT_SCOPE)
 endfunction()
