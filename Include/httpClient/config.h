f06571be81d27c9668cd6f90e8fb87929b6eee99
Thomas Culotta <tculotta@microsoft.com>
2021-06-18 17:14:06 -0700

    Task 33321557: Streamline XSAPI thunk dll generation and ship it in the GDK

--- Include/httpClient/config.h
+++ Include/httpClient/config.h
@@ -33,17 +33,16 @@
 
     #if defined(_GAMING_DESKTOP) || defined(_GAMING_XBOX) || defined(_GAMING_XBOX_XBOXONE) || defined(_GAMING_XBOX_SCARLETT)
         #define HC_PLATFORM HC_PLATFORM_GDK
-    #elif !defined(WINAPI_FAMILY) || (WINAPI_FAMILY == WINAPI_FAMILY_DESKTOP_APP)
-        #define HC_PLATFORM HC_PLATFORM_WIN32
-        #include <windef.h>
         
-        #if !defined(APP_LOCAL_DEVICE_ID_SIZE)
+        #if !defined(NTDDI_WIN10_VB)
             #define APP_LOCAL_DEVICE_ID_SIZE 32
             typedef struct APP_LOCAL_DEVICE_ID
             {
-	            BYTE value[APP_LOCAL_DEVICE_ID_SIZE];
+                BYTE value[APP_LOCAL_DEVICE_ID_SIZE];
             } APP_LOCAL_DEVICE_ID;
         #endif
+    #elif !defined(WINAPI_FAMILY) || (WINAPI_FAMILY == WINAPI_FAMILY_DESKTOP_APP)
+        #define HC_PLATFORM HC_PLATFORM_WIN32
     #elif WINAPI_FAMILY == WINAPI_FAMILY_PC_APP && _WIN32_WINNT >= _WIN32_WINNT_WIN10
         #define HC_PLATFORM HC_PLATFORM_UWP
     #elif WINAPI_FAMILY == WINAPI_FAMILY_TV_APP || WINAPI_FAMILY == WINAPI_FAMILY_TV_TITLE
