b3906e913a1bcf93ff692941d8f7dcf786d60257
Jason Sandlin <jasonsa@microsoft.com>
2021-07-13 14:49:58 -0700

    Adding assert for GDK console to note that SSL validation is enforced on RETAIL sandboxes (#589)
    * Adding assert for GDK console to note that SSL validation is enforced on RETAIL sandboxes
    
    * Updating comment

--- Source/Global/global.h
+++ Source/Global/global.h
@@ -79,6 +79,7 @@ public:
 #if HC_PLATFORM == HC_PLATFORM_GDK
     bool m_networkInitialized{ true };
     HMODULE m_networkModule{ nullptr };
+    bool m_disableAssertsForSSLValidationInDevSandboxes{ false };
 #endif
 
 #if !HC_NOWEBSOCKETS
