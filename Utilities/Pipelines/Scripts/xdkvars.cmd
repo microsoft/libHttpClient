b0bc8cd369e76ace754eda7a19176bd81dd108e5
Jason Sandlin <jasonsa@microsoft.com>
2021-11-08 12:00:48 -0800

    Updating to build images and build process (#630)
    * Updating to build images and build process
    
    * Updates

--- Utilities/Pipelines/Scripts/xdkvars.cmd
+++ Utilities/Pipelines/Scripts/xdkvars.cmd
@@ -2,6 +2,4 @@ SET XdkEditionFinal=%XDKEDITION%
 SET MSBuildVersion=15.8
 set DurangoXdkInstallPath=C:\Program Files (x86)\Microsoft Durango XDK\
 
-robocopy /NJS /NJH /MT:16 /S /NP "%BUILD_STAGINGDIRECTORY%\sdk.buildtools\buildMachine\Durango" "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\VC\VCTargets\Platforms\Durango"
-
 set
