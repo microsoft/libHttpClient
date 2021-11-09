d4fd4455d2a10106ad8a9a2bb148c590665bf7a9
heba <xlivexdevx@V-TKLIMEK-CON.redmond.corp.microsoft.com>
2021-11-09 08:45:32 -0800

    Fixing more iOS issues

--- Source/HTTP/Apple/http_apple.h
+++ Source/HTTP/Apple/http_apple.h
@@ -1,27 +1,10 @@
+
 // Copyright (c) Microsoft Corporation
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#pragma once
-#import <Foundation/Foundation.h>
+#include "pch.h"
 
 NAMESPACE_XBOX_HTTP_CLIENT_BEGIN
-
-class http_task_apple
-{
-public:
-    http_task_apple(_Inout_ XAsyncBlock* asyncBlock, _In_ HCCallHandle call);
-    bool initiate_request();
-
-private:
-    void completion_handler(NSURLResponse* response, NSError* error);
-    
-    HCCallHandle m_call; // non owning
-    XAsyncBlock* m_asyncBlock; // non owning
-    
-    NSURLSession* m_session;
-    NSURLSessionTask* m_sessionTask;
-};
-
 void AppleHttpCallPerformAsync(
     _In_ HCCallHandle call,
     _Inout_ XAsyncBlock* asyncBlock,
