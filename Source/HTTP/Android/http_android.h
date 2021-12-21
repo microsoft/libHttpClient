fe4e4eaa3b9038d6efff232ee32b3591ffb3e664
Sahil Ashar <saashar@microsoft.com>
2021-12-21 15:22:04 -0600

    Fix for Android JNI link bug (#648)

---
+++ Source/HTTP/Android/http_android.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <jni.h>
+
+// These JNI functions are being exposed by this header file so that they
+// can be referenced in another .cpp file. This is so the compiler is forced
+// to compile these functions and prevent them from being optimized out.
+
+extern "C"
+{
+
+JNIEXPORT void JNICALL Java_com_xbox_httpclient_HttpClientRequest_OnRequestCompleted(
+    JNIEnv * /* env */,
+    jobject /* instance */,
+    jlong call,
+    jobject response
+);
+
+JNIEXPORT void JNICALL Java_com_xbox_httpclient_HttpClientRequest_OnRequestFailed(
+        JNIEnv* env,
+        jobject /* instance */,
+        jlong call,
+        jstring errorMessage,
+        jstring stackTrace,
+        jboolean isNoNetwork
+);
+
+JNIEXPORT jint JNICALL Java_com_xbox_httpclient_HttpClientRequestBody_00024NativeInputStream_nativeRead(
+        JNIEnv* env,
+        jobject /* instance */,
+        jlong callHandle,
+        jlong srcOffset,
+        jbyteArray dst,
+        jlong dstOffset,
+        jlong bytesAvailable
+);
+
+JNIEXPORT void JNICALL Java_com_xbox_httpclient_HttpClientResponse_00024NativeOutputStream_nativeWrite(
+        JNIEnv* env,
+        jobject /* instance */,
+        jlong callHandle,
+        jbyteArray src,
+        jint sourceOffset,
+        jint sourceLength
+);
+
+}
