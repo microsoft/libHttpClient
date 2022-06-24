2b6d82b01747c88a36bf30282a9691bc0657d26b
Sahil <saashar@microsoft.com>
2022-06-24 11:41:39 -0700

    Updating namespacing

--- Source/HTTP/Android/http_android.h
+++ Source/HTTP/Android/http_android.h
@@ -6,6 +6,8 @@
 // can be referenced in another .cpp file. This is so the compiler is forced
 // to compile these functions and prevent them from being optimized out.
 
+NAMESPACE_XBOX_HTTP_CLIENT_BEGIN
+
 extern "C"
 {
 
@@ -52,3 +54,5 @@ JNIEXPORT void JNICALL Java_com_xbox_httpclient_HttpClientResponse_00024NativeOu
 );
 
 }
+
+NAMESPACE_XBOX_HTTP_CLIENT_END
