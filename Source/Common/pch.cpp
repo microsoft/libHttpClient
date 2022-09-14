1e908b455d92cae9e2492bf0b05068b984849df9
Jason Sandlin <jasonsa@microsoft.com>
2022-09-13 17:50:01 -0700

    Enable verbose logs on release builds

--- Source/Common/pch.cpp
+++ Source/Common/pch.cpp
@@ -3,5 +3,5 @@
 
 #include "pch.h"
 
-HC_DEFINE_TRACE_AREA(HTTPCLIENT, HCTraceLevel::Information);
-HC_DEFINE_TRACE_AREA(WEBSOCKET, HCTraceLevel::Information);
+HC_DEFINE_TRACE_AREA(HTTPCLIENT, HCTraceLevel::Verbose);
+HC_DEFINE_TRACE_AREA(WEBSOCKET, HCTraceLevel::Verbose);
