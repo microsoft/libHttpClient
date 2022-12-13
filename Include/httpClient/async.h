11abd0f3be701311a996e4b893089393aa5d12b5
Luke Gehorsam <luke@heroiclabs.com>
2022-12-13 12:11:15 -0500

    remove include prefixing for static apple libs

--- Include/httpClient/async.h
+++ Include/httpClient/async.h
@@ -6,15 +6,9 @@
 #include <httpClient/config.h>
 #include <httpClient/pal.h>
 
-#if (!defined(HC_LINK_STATIC) || HC_LINK_STATIC == 0) && HC_PLATFORM_IS_APPLE
-#include <httpClient/XAsync.h>
-#include <httpClient/XAsyncProvider.h>
-#include <httpClient/XTaskQueue.h>
-#else
 #include <XAsync.h>
 #include <XAsyncProvider.h>
 #include <XTaskQueue.h>
-#endif
 
 #if HC_PLATFORM == HC_PLATFORM_ANDROID
 #include <httpClient/async_jvm.h>
