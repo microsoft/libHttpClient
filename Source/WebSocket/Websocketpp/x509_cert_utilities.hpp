8de3ae9a212c4c1dc30f135a7072a31f90569456
SebastianPD-XB <46662157+SebastianPD@users.noreply.github.com>
2023-03-10 15:34:25 -0800

    Update x509_cert_utilities.hpp
    use elif and corrected to use else

--- Source/WebSocket/Websocketpp/x509_cert_utilities.hpp
+++ Source/WebSocket/Websocketpp/x509_cert_utilities.hpp
@@ -57,7 +57,7 @@ namespace xbox { namespace httpclient {
 
 #if HC_PLATFORM == HC_PLATFORM_LINUX
     static bool verify_X509_cert_chain(asio::ssl::verify_context& verifyCtx, const http_internal_string& hostName);
-#elif
+#else
     static bool verify_X509_cert_chain(const http_internal_vector<http_internal_string> &certChain, const http_internal_string &hostName);
 #endif
 
