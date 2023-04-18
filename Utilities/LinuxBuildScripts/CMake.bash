1ef6eac8860db685222c2a356de1f71af9521837
SebastianPD <sebastian.perezdelgado@gmail.com>
2023-04-17 17:11:34 -0700

    updated libhc cmake and made a better bash script

--- Utilities/LinuxBuildScripts/CMake.bash
+++ Utilities/LinuxBuildScripts/CMake.bash
@@ -1,54 +1,31 @@
 #!/bin/bash
-echo 
-read -p "Enter R for Release " VAR1
-VAR2="R"
-if [ "$VAR1" = "$VAR2" ]; then
-  echo "Release build Starting"
-
-    #make libssl
-    mkdir ./../../Build/libssl.Linux/build
-    cmake -S ./../CMake/Linux/openssl/libssl -B ./../../Build/libssl.Linux/build -D CMAKE_BUILD_TYPE=Release
-    make -C ./../../Build/libssl.Linux/build
-    mkdir -p ./../../Binaries/Release/x64/libssl.Linux
-    cp ./../../Build/libssl.Linux/build/libssl.Linux.a ./../../Binaries/Release/x64/libssl.Linux
-    make -C ./../../Build/libssl.Linux/build clean
-    #make libcrypto
-    mkdir ./../../Build/libcrypto.Linux/build
-    cmake -S ./../CMake/Linux/openssl/libcrypto -B ./../../Build/libcrypto.Linux/build -D CMAKE_BUILD_TYPE=Release
-    make -C ./../../Build/libcrypto.Linux/build
-    mkdir -p ./../../Binaries/Release/x64/libcrypto.Linux
-    cp ./../../Build/libcrypto.Linux/build/libcrypto.Linux.a ./../../Binaries/Release/x64/libcrypto.Linux
-    make -C ./../../Build/libcrypto.Linux/build clean
-    #make libHttpClient
-    mkdir ./../../Build/libHttpClient.Linux.C/build
-    cmake -S ./../CMake/Linux/libHttpClient -B ./../../Build/libHttpClient.Linux.C/build -D CMAKE_BUILD_TYPE=Release
-    make -C ./../../Build/libHttpClient.Linux.C/build
-    mkdir -p ./../../Binaries/Release/x64/libHttpClient.Linux.C
-    cp ./../../Build/libHttpClient.Linux.C/build/libHttpClient.Linux.C.a ./../../Binaries/Release/x64/libHttpClient.Linux.C
-    make -C ./../../Build/libHttpClient.Linux.C/build clean
-
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+read -p "Enter R for Release " INPUTVAR
+if [ "$INPUTVAR" = "R" ]; then
+    echo "Release build Starting"
+    CONFIGVAR="Release"
 else
-  echo "Debug build Starting"
-
-    #make libssl
-    mkdir ./../../Build/libssl.Linux/build
-    cmake -S ./../CMake/Linux/openssl/libssl -B ./../../Build/libssl.Linux/build -D CMAKE_BUILD_TYPE=Debug
-    make -C ./../../Build/libssl.Linux/build
-    mkdir -p ./../../Binaries/Debug/x64/libssl.Linux
-    cp ./../../Build/libssl.Linux/build/libssl.Linux.a ./../../Binaries/Debug/x64/libssl.Linux
-    make -C ./../../Build/libssl.Linux/build clean
-    #make libcrypto
-    mkdir ./../../Build/libcrypto.Linux/build
-    cmake -S ./../CMake/Linux/openssl/libcrypto -B ./../../Build/libcrypto.Linux/build -D CMAKE_BUILD_TYPE=Debug
-    make -C ./../../Build/libcrypto.Linux/build
-    mkdir -p ./../../Binaries/Debug/x64/libcrypto.Linux
-    cp ./../../Build/libcrypto.Linux/build/libcrypto.Linux.a ./../../Binaries/Debug/x64/libcrypto.Linux
-    make -C ./../../Build/libcrypto.Linux/build clean
-    #make libHttpClient
-    mkdir ./../../Build/libHttpClient.Linux.C/build
-    cmake -S ./../CMake/Linux/libHttpClient -B ./../../Build/libHttpClient.Linux.C/build -D CMAKE_BUILD_TYPE=Debug
-    make -C ./../../Build/libHttpClient.Linux.C/build
-    mkdir -p ./../../Binaries/Debug/x64/libHttpClient.Linux.C
-    cp ./../../Build/libHttpClient.Linux.C/build/libHttpClient.Linux.C.a ./../../Binaries/Debug/x64/libHttpClient.Linux.C
-    make -C ./../../Build/libHttpClient.Linux.C/build clean
+    echo "Debug build Starting"
+    CONFIGVAR="Debug"
 fi
+#make libssl
+mkdir "$SCRIPT_DIR"/../../Build/libssl.Linux/build
+cmake -S "$SCRIPT_DIR"/../CMake/Linux/openssl/libssl -B "$SCRIPT_DIR"/../../Build/libssl.Linux/build -D CMAKE_BUILD_TYPE="$CONFIGVAR"
+make -C "$SCRIPT_DIR"/../../Build/libssl.Linux/build
+mkdir -p "$SCRIPT_DIR"/../../Binaries/"$CONFIGVAR"/x64/libssl.Linux
+cp "$SCRIPT_DIR"/../../Build/libssl.Linux/build/libssl.Linux.a "$SCRIPT_DIR"/../../Binaries/"$CONFIGVAR"/x64/libssl.Linux
+make -C "$SCRIPT_DIR"/../../Build/libssl.Linux/build clean
+#make libcrypto
+mkdir "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build
+cmake -S "$SCRIPT_DIR"/../CMake/Linux/openssl/libcrypto -B "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build -D CMAKE_BUILD_TYPE="$CONFIGVAR"
+make -C "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build
+mkdir -p "$SCRIPT_DIR"/../../Binaries/"$CONFIGVAR"/x64/libcrypto.Linux
+cp "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build/libcrypto.Linux.a "$SCRIPT_DIR"/../../Binaries/"$CONFIGVAR"/x64/libcrypto.Linux
+make -C "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build clean
+#make libHttpClient
+mkdir "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build
+cmake -S "$SCRIPT_DIR"/../CMake/Linux/libHttpClient -B "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build -D CMAKE_BUILD_TYPE="$CONFIGVAR"
+make -C "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build
+mkdir -p "$SCRIPT_DIR"/../../Binaries/"$CONFIGVAR"/x64/libHttpClient.Linux.C
+cp "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build/libHttpClient.Linux.C.a "$SCRIPT_DIR"/../../Binaries/"$CONFIGVAR"/x64/libHttpClient.Linux.C
+make -C "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build clean
