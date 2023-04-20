902f64f74263991446b68a9d0669442c7f6cb5f3
SebastianPD <sebastian.perezdelgado@gmail.com>
2023-04-20 15:38:04 -0700

    bash script and cmake optimizations
    used cmake to reduce number of steps in bash script process. Used ninja to help with compilation time.

--- Utilities/LinuxBuildScripts/CMake.bash
+++ Utilities/LinuxBuildScripts/CMake.bash
@@ -9,23 +9,11 @@ else
     CONFIGVAR="Debug"
 fi
 #make libssl
-mkdir "$SCRIPT_DIR"/../../Build/libssl.Linux/build
-cmake -S "$SCRIPT_DIR"/../CMake/Linux/openssl/libssl -B "$SCRIPT_DIR"/../../Build/libssl.Linux/build -D CMAKE_BUILD_TYPE="$CONFIGVAR"
-make -C "$SCRIPT_DIR"/../../Build/libssl.Linux/build
-mkdir -p "$SCRIPT_DIR"/../../Binaries/"$CONFIGVAR"/x64/libssl.Linux
-cp "$SCRIPT_DIR"/../../Build/libssl.Linux/build/libssl.Linux.a "$SCRIPT_DIR"/../../Binaries/"$CONFIGVAR"/x64/libssl.Linux
-make -C "$SCRIPT_DIR"/../../Build/libssl.Linux/build clean
+cmake -S "$SCRIPT_DIR"/../CMake/Linux/openssl/libssl -B "$SCRIPT_DIR"/../../Build/libssl.Linux/build -D CMAKE_BUILD_TYPE=$CONFIGVAR -G "Ninja"
+ninja -C "$SCRIPT_DIR"/../../Build/libssl.Linux/build
 #make libcrypto
-mkdir "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build
-cmake -S "$SCRIPT_DIR"/../CMake/Linux/openssl/libcrypto -B "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build -D CMAKE_BUILD_TYPE="$CONFIGVAR"
-make -C "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build
-mkdir -p "$SCRIPT_DIR"/../../Binaries/"$CONFIGVAR"/x64/libcrypto.Linux
-cp "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build/libcrypto.Linux.a "$SCRIPT_DIR"/../../Binaries/"$CONFIGVAR"/x64/libcrypto.Linux
-make -C "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build clean
+cmake -S "$SCRIPT_DIR"/../CMake/Linux/openssl/libcrypto -B "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build -D CMAKE_BUILD_TYPE=$CONFIGVAR -G "Ninja"
+ninja -C "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build
 #make libHttpClient
-mkdir "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build
-cmake -S "$SCRIPT_DIR"/../CMake/Linux/libHttpClient -B "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build -D CMAKE_BUILD_TYPE="$CONFIGVAR"
-make -C "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build
-mkdir -p "$SCRIPT_DIR"/../../Binaries/"$CONFIGVAR"/x64/libHttpClient.Linux.C
-cp "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build/libHttpClient.Linux.C.a "$SCRIPT_DIR"/../../Binaries/"$CONFIGVAR"/x64/libHttpClient.Linux.C
-make -C "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build clean
+cmake -S "$SCRIPT_DIR"/../CMake/Linux/libHttpClient -B "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build -D CMAKE_BUILD_TYPE=$CONFIGVAR -G "Ninja"
+ninja -C "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build
