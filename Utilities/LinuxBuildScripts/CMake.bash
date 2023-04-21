13e0ee6a054a7f0a7023d72e1f933e00d644becb
SebastianPD <sebastian.perezdelgado@gmail.com>
2023-04-21 16:20:42 -0700

    Update CMake.bash

--- Utilities/LinuxBuildScripts/CMake.bash
+++ Utilities/LinuxBuildScripts/CMake.bash
@@ -15,11 +15,11 @@ if [ "$1" != "nocurl" ] && [ "$2" != "nocurl" ]; then
 fi
 
 #make libssl
-cmake -S "$SCRIPT_DIR"/../CMake/Linux/openssl/libssl -B "$SCRIPT_DIR"/../../Build/libssl.Linux/build -D CMAKE_BUILD_TYPE=$CONFIGVAR -G "Ninja"
-ninja -C "$SCRIPT_DIR"/../../Build/libssl.Linux/build
+sudo cmake -S "$SCRIPT_DIR"/../CMake/Linux/openssl/libssl -B "$SCRIPT_DIR"/../../Build/libssl.Linux/build -D CMAKE_BUILD_TYPE=$CONFIGVAR -G "Ninja"
+sudo ninja -C "$SCRIPT_DIR"/../../Build/libssl.Linux/build
 #make libcrypto
-cmake -S "$SCRIPT_DIR"/../CMake/Linux/openssl/libcrypto -B "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build -D CMAKE_BUILD_TYPE=$CONFIGVAR -G "Ninja"
-ninja -C "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build
+sudo cmake -S "$SCRIPT_DIR"/../CMake/Linux/openssl/libcrypto -B "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build -D CMAKE_BUILD_TYPE=$CONFIGVAR -G "Ninja"
+sudo ninja -C "$SCRIPT_DIR"/../../Build/libcrypto.Linux/build
 #make libHttpClient
-cmake -S "$SCRIPT_DIR"/../CMake/Linux/libHttpClient -B "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build -D CMAKE_BUILD_TYPE=$CONFIGVAR -G "Ninja"
-ninja -C "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build
+sudo cmake -S "$SCRIPT_DIR"/../CMake/Linux/libHttpClient -B "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build -D CMAKE_BUILD_TYPE=$CONFIGVAR -G "Ninja"
+sudo ninja -C "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build
