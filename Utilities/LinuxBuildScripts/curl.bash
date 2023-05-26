e50eb66f2092f5699f93c46a5b94e430fea54e5e
SebastianPD <sebastian.perezdelgado@gmail.com>
2023-05-26 15:19:15 -0700

    revert back to how it worked before

--- Utilities/LinuxBuildScripts/curl.bash
+++ Utilities/LinuxBuildScripts/curl.bash
@@ -31,13 +31,14 @@ sudo apt-get install libtool
 
 pushd "$SCRIPT_DIR"/../../External/curl
 autoreconf
+-./configure
 
 if [ "$CONFIGURATION" = "Debug" ]; then
     # make libcrypto and libssl
-    -./configure --disable-dependency-tracking -with-ssl --enable-symbol-hiding --disable-shared --enable-debug
+    ./configure --disable-dependency-tracking -with-ssl --enable-symbol-hiding --disable-shared --enable-debug
 else
     # make libcrypto and libssl
-    -./configure --disable-dependency-tracking -with-ssl --enable-symbol-hiding --disable-shared --disable-debug
+    ./configure --disable-dependency-tracking -with-ssl --enable-symbol-hiding --disable-shared --disable-debug
 fi
 
 make
