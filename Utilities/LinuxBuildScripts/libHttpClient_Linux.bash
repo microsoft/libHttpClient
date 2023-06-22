38cc8ed17893d8d9f8028f5cbb9521cbf21f1bfb
Sebastian Perez-Delgado <46662157+SebastianPD@users.noreply.github.com>
2023-06-22 11:24:14 -0700

    LibHC linux build curl using libssl in repo submodule (#752)
    * change build order
    
    * change curl flags
    
    * change pipeline to reflect dependency changes
    
    * revised bash scripts
    
    * fix white space nit
    
    * restructure build scripts and pipeline
    
    ---------
    
    Co-authored-by: Sahil Ashar <saashar@microsoft.com>

--- Utilities/LinuxBuildScripts/libHttpClient_Linux.bash
+++ Utilities/LinuxBuildScripts/libHttpClient_Linux.bash
@@ -10,6 +10,7 @@ POSITIONAL_ARGS=()
 # Default configurations
 CONFIGURATION="Release"
 BUILD_CURL=true
+BUILD_SSL=true
 
 while [[ $# -gt 0 ]]; do
   case $1 in
@@ -20,6 +21,10 @@ while [[ $# -gt 0 ]]; do
       ;;
     -nc|--nocurl)
       BUILD_CURL=false
+      shift
+      ;;
+    -ns|--nossl)
+      BUILD_SSL=false
       shift
       ;;
     -*|--*)
@@ -38,15 +43,18 @@ set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 log "CONFIGURATION  = ${CONFIGURATION}"
 log "BUILD CURL     = ${SEARCHPATH}"
 
+# make libcrypto and libssl
+if [ "$BUILD_SSL" = true ]; then
+    log "Building SSL"
+    sed -i -e 's/\r$//' "$SCRIPT_DIR"/openssl.bash
+    bash "$SCRIPT_DIR"/openssl.bash -c "$CONFIGURATION"
+fi
+
 if [ "$BUILD_CURL" = true ]; then
     log "Building cURL"
     sed -i -e 's/\r$//' "$SCRIPT_DIR"/curl.bash
     bash "$SCRIPT_DIR"/curl.bash -c "$CONFIGURATION"
 fi
-
-# make libcrypto and libssl
-sed -i -e 's/\r$//' "$SCRIPT_DIR"/openssl.bash
-bash "$SCRIPT_DIR"/openssl.bash -c "$CONFIGURATION"
 
 # make libHttpClient
 sudo cmake -S "$SCRIPT_DIR"/../CMake/Linux/libHttpClient -B "$SCRIPT_DIR"/../../Build/libHttpClient.Linux.C/build -D CMAKE_BUILD_TYPE=$CONFIGURATION -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++  -DCMAKE_CXX_FLAGS="-fvisibility=hidden" -DCMAKE_C_FLAGS="-fvisibility=hidden"
