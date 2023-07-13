b8c1edce9bf996c02a204b66d1c22812c65c27c0
John LaFontaine <johlafo@microsoft.com>
2023-07-13 07:59:59 -0700

    Fix leak due to multiple inheritance + custom deleter

--- Source/Platform/Win32/PlatformComponents_Win32.cpp
+++ Source/Platform/Win32/PlatformComponents_Win32.cpp
@@ -4,56 +4,6 @@
 
 NAMESPACE_XBOX_HTTP_CLIENT_BEGIN
 
-class WinHttp_HttpProvider : public IHttpProvider
-{
-public:
-    WinHttp_HttpProvider(std::shared_ptr<WinHttpProvider> provider);
-
-    HRESULT PerformAsync(
-        HCCallHandle callHandle,
-        XAsyncBlock* async
-    ) noexcept override;
-
-private:
-    std::shared_ptr<WinHttpProvider> m_sharedProvider;
-};
-
-#if !HC_NOWEBSOCKETS
-class WinHttp_WebSocketProvider : public IWebSocketProvider
-{
-public:
-    WinHttp_WebSocketProvider(std::shared_ptr<WinHttpProvider> provider);
-
-    HRESULT ConnectAsync(
-        String const& uri,
-        String const& subprotocol,
-        HCWebsocketHandle websocketHandle,
-        XAsyncBlock* async
-    ) noexcept override;
-
-    HRESULT SendAsync(
-        HCWebsocketHandle websocketHandle,
-        const char* message,
-        XAsyncBlock* async
-    ) noexcept override;
-
-    HRESULT SendBinaryAsync(
-        HCWebsocketHandle websocketHandle,
-        const uint8_t* payloadBytes,
-        uint32_t payloadSize,
-        XAsyncBlock* asyncBlock
-    ) noexcept override;
-
-    HRESULT Disconnect(
-        HCWebsocketHandle websocketHandle,
-        HCWebSocketCloseStatus closeStatus
-    ) noexcept override;
-
-private:
-    std::shared_ptr<WinHttpProvider> m_sharedProvider;
-};
-#endif
-
 HRESULT PlatformInitialize(PlatformComponents& components, HCInitArgs* initArgs)
 {
     // We don't expect initArgs on Win32
@@ -72,57 +22,5 @@ HRESULT PlatformInitialize(PlatformComponents& components, HCInitArgs* initArgs)
 
     return S_OK;
 }
-
-WinHttp_HttpProvider::WinHttp_HttpProvider(std::shared_ptr<WinHttpProvider> provider) : m_sharedProvider{ std::move(provider) }
-{
-}
-
-HRESULT WinHttp_HttpProvider::PerformAsync(HCCallHandle callHandle, XAsyncBlock* async) noexcept
-{
-    return m_sharedProvider->PerformAsync(callHandle, async);
-}
-
-#if !HC_NOWEBSOCKETS
-WinHttp_WebSocketProvider::WinHttp_WebSocketProvider(std::shared_ptr<WinHttpProvider> provider) : m_sharedProvider{ std::move(provider) }
-{
-}
-
-HRESULT WinHttp_WebSocketProvider::ConnectAsync(
-    String const& uri,
-    String const& subprotocol,
-    HCWebsocketHandle websocketHandle,
-    XAsyncBlock* async
-) noexcept
-{
-    return m_sharedProvider->ConnectAsync(uri, subprotocol, websocketHandle, async);
-}
-
-HRESULT WinHttp_WebSocketProvider::SendAsync(
-    HCWebsocketHandle websocketHandle,
-    const char* message,
-    XAsyncBlock* async
-) noexcept
-{
-    return m_sharedProvider->SendAsync(websocketHandle, message, async);
-}
-
-HRESULT WinHttp_WebSocketProvider::SendBinaryAsync(
-    HCWebsocketHandle websocketHandle,
-    const uint8_t* payloadBytes,
-    uint32_t payloadSize,
-    XAsyncBlock* asyncBlock
-) noexcept
-{
-    return m_sharedProvider->SendBinaryAsync(websocketHandle, payloadBytes, payloadSize, asyncBlock);
-}
-
-HRESULT WinHttp_WebSocketProvider::Disconnect(
-    HCWebsocketHandle websocketHandle,
-    HCWebSocketCloseStatus closeStatus
-) noexcept
-{
-    return m_sharedProvider->Disconnect(websocketHandle, closeStatus);
-}
-#endif // !HC_NOWEBSOCKETS
 
 NAMESPACE_XBOX_HTTP_CLIENT_END
