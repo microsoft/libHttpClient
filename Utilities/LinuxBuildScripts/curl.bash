34080f8fb10452f6ef3b25be3fcc3772ae7632f3
SebastianPD <sebastian.perezdelgado@gmail.com>
2023-07-17 16:06:57 -0700

    forgot space in debug
    gcc flag works just had a typo in debug

--- Utilities/LinuxBuildScripts/curl.bash
+++ Utilities/LinuxBuildScripts/curl.bash
@@ -33,7 +33,7 @@ autoreconf -fi "$SCRIPT_DIR"/../../External/curl
 
 if [ "$CONFIGURATION" = "Debug" ]; then
     # make libcrypto and libssl
-    LDFLAGS=-Wl,-R/usr/local/ssl/lib ./configure --disable-shared --disable-dependency-tracking --with-openssl--enable-symbol-hiding --enable-debug
+    LDFLAGS=-Wl,-R/usr/local/ssl/lib ./configure --disable-shared --disable-dependency-tracking --with-openssl --enable-symbol-hiding --enable-debug
 else
     # make libcrypto and libssl
     LDFLAGS=-Wl,-R/usr/local/ssl/lib ./configure --disable-shared --disable-dependency-tracking --with-openssl --enable-symbol-hiding --disable-debug
