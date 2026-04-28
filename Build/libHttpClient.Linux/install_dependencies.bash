c987fb7bc4121be475fc2d2040badf41e2b3a1fc
Scott Munro <scmunro@microsoft.com>
2026-04-27 21:52:01 -0700

    LibHttpClient.Linux: refactor dependency install to optional step and eliminate forced sudo calls (#963)
    LibHttpClient's linux build had `sudo` authorized calls sprinkled throughout its build by default to install dependencies. Generally, this is an anti-pattern. It means that to ever build libHttpClient for linux you need to have sudo privileges even if all required dependencies are installed. sudo should be isolated to and reserved for operations which require elevations. General builds should not require elevation.
    
    Callers can continue to retain the old behavior by passing `--install-dependencies` to the old build script.
    
    I've also eliminated some unnecessary sudo usage from the openssl build. it was primarily there to give write access to files in the global /usr/local folder. Instead of installing openssl build results there, install them to a local temp folder instead and the need for sudo goes away.

---
+++ Build/libHttpClient.Linux/install_dependencies.bash
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+POSITIONAL_ARGS=()
+DO_INSTALL=true
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --check)
+      DO_INSTALL=false
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+build_dependencies=(clang make autoconf automake libtool)
+library_dependencies=(zlib1g zlib1g-dev)
+
+if [ "$DO_INSTALL" = false ]; then
+
+  dependencies_missing=false
+  for dep in "${build_dependencies[@]}" "${library_dependencies[@]}"; do
+    if ! dpkg -s "$dep" &> /dev/null; then
+      echo "Missing dependency: $dep"
+      dependencies_missing=true
+    else
+      echo "Dependency installed: $dep"
+    fi
+  done
+
+  if [ "$dependencies_missing" = true ]; then
+    exit 1
+  fi
+
+  echo "All dependencies are installed"
+  exit 0
+fi
+
+echo "Installing dependencies..."
+echo "Build dependencies: ${build_dependencies[*]}"
+echo "Library dependencies: ${library_dependencies[*]}"
+
+sudo hwclock --hctosys
+sudo apt-get update
+sudo apt-get -y install "${build_dependencies[@]}" "${library_dependencies[@]}"
