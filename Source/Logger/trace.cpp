cf888aaf9163fd8ada17a64d4a978ef9d560ae11
Gent Semaj <me@ycc.dev>
2026-09-23 18:16:13 -0700

    Internal linkage for `g_hTraceLoggingProvider` (#1024)

--- Source/Logger/trace.cpp
+++ Source/Logger/trace.cpp
@@ -73,11 +73,16 @@ int vstprintf_s(char(&buffer)[SIZE], _Printf_format_string_ char const* format, 
 #endif
 
 #if HC_PLATFORM_IS_MICROSOFT
-TRACELOGGING_DEFINE_PROVIDER(
-    g_hTraceLoggingProvider,
+// TRACELOGGING_DEFINE_PROVIDER_STORAGE and the const decl below are used instead of
+// TRACELOGGING_DEFINE_PROVIDER to keep `g_hTraceLoggingProvider` linkage internal.
+// This avoids link-time collisions in any static consumers (`g_hTraceLoggingProvider` is very common).
+TRACELOGGING_DEFINE_PROVIDER_STORAGE(
+    g_lhcTraceLoggingProviderStorage,
     "libHttpClient",
     (0x2b6274b2, 0x159f, 0x4e6f, 0xb8, 0x0, 0xd, 0x17, 0x0, 0x9e, 0x14, 0x7a) // {2B6274B2-159F-4E6F-B800-0D17009E147A}
 );
+
+const TraceLoggingHProvider g_hTraceLoggingProvider = &g_lhcTraceLoggingProviderStorage;
 
 
 // Helper method to ensure our ETW Provider never gets registered twice. Registering an ETW provider twice may result
